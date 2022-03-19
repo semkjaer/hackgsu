70091dbdf7e7bf9959bcc10edbbd7c168cbacaa1
Sem Kjaer <semkjaer@hotmail.nl>
2022-03-19 14:24:20 +0100

    Update math_concepts.py

--- math_concepts.py
+++ math_concepts.py
@@ -1,6 +1,7 @@
 import requests
 import re
 
+# queries wikidata.org SPARQL endpoint for mathematical concepts
 url = 'https://query.wikidata.org/sparql'
 query = '''
 SELECT DISTINCT ?article ?label
@@ -22,10 +23,13 @@ data = r.text
 
 with open("temp.csv", "w", newline="", encoding='utf-8') as f:
     for x in data:
+        # there's random cases of " in text so we remove those
         if not x == '"':
             f.write(x)
 
+# default format uses ',' as a delimiter but sometimes there's a ',' in a url so we change the delimiter to ';'
 with open("temp.csv", "r", encoding="utf-8") as fin:
     with open("math_concepts.csv", "w", encoding="utf-8") as fout:
         for row in fin:
+            # cases of ',' in urls are always followed by '_' so we take the first instance of ',' that isn't followed by '_' and replace it
             fout.write(re.sub(',(?!_)', ';', row, 1))
